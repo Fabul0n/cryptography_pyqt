0edd6a7c44d9b8c63b3a06ae897f70bde65da799
Fabulon <fabulon@mail.ru>
2025-02-22 12:37:26 +0700

    implemented Caesar cipher. Changed some namings

---
+++ ciphers/caesar.py
@@ -0,0 +1,45 @@
+from ciphers.base import BaseCipher
+from utils.alphabets import alphabets
+
+class Caesar(BaseCipher):
+    def __init__(self, message, key):
+        self.message = message
+        self.key = key
+
+    def set_key(self, key):
+        self.key = key
+
+    def set_message(self, message):
+        self.message = message
+
+    def encode(self):
+        """
+        :return: возвращает сообщение закодированное методом Цезаря
+        """
+        result: str = ''
+        is_encoded: bool = False
+        for char in self.message:
+            for alphabet in alphabets.values():
+                if char in alphabet:
+                    result += alphabet[(alphabet.find(char) + self.key) % len(alphabet)]
+                    is_encoded = True
+                    break
+            if not is_encoded:
+                result += char
+        return result
+
+    def decode(self):
+        """
+        :return: возвращает сообщение декодированное методом Цезаря
+        """
+        result: str = ''
+        is_decoded: bool = False
+        for char in self.message:
+            for alphabet in alphabets.values():
+                if char in alphabet:
+                    result += alphabet[(alphabet.find(char) - self.key) % len(alphabet)]
+                    is_decoded = True
+                    break
+            if not is_decoded:
+                result += char
+        return result
--- utils/get_alphabets.py
+++
@@ -1,4 +0,0 @@
-import json
-
-with open('C:\\Users\\mixas\\Documents\\repos\\Projects\\cryptography_pyqt\\utils\\alphabets,json', 'r', encoding='utf-8') as f:
-    alphabets: dict[str, str] = json.load(f)
