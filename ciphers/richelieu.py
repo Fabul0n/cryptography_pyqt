94de9e4bcde4aaf7d9fbe95b8960d269cea0c439
fabulon <fabulon>
2025-02-26 12:40:43 +0700

    first three. Final version

--- ciphers/richelieu.py
+++ ciphers/richelieu.py
@@ -1,5 +1,4 @@
 from ciphers.base import BaseCipher
-from utils.alphabets import alphabets
 
 class Richelieu(BaseCipher):
     def __init__(self, message, key):
@@ -40,7 +39,9 @@ class Richelieu(BaseCipher):
         ls = self.key.split(')')
         left_bracket_count = 0
         char_count1 = 0
-        char_count2 = 0
+
+        if ls[-1] != '':
+            raise ValueError
 
         for _ in ls[:-1]:
             if len(_) == 0:
@@ -51,73 +52,31 @@ class Richelieu(BaseCipher):
             self._validate_part(lss)
             char_count1 += len(lss)
 
-        if left_bracket_count != len(ls)-1:
+        if left_bracket_count != len(ls)-1 or left_bracket_count == 0:
             raise ValueError
-        
-        for char in self.message:
-            for alphabet in alphabets.values():
-                if char in alphabet:
-                    char_count2 += 1
-                    break
 
-        if char_count1 > char_count2:
+        if char_count1 > len(self.message):
             raise ValueError
 
     def encode(self):
         self._validate_key()
         result = ''
-        subresult = ''
         indent = 0
         parts = self._parse_key()
-        all_chars = ''
-        for char in self.message:
-            for alphabet in alphabets.values():
-                if char in alphabet:
-                    all_chars += char
         for part in parts:
             for _ in part:
-                subresult += all_chars[indent + _ - 1]
+                result += self.message[indent + _ - 1]
             indent += len(part)
-        subresult += all_chars[len(subresult):]
-        i = 0
-        for char in self.message:
-            is_encoded = False
-            for alphabet in alphabets.values():
-                if char in alphabet:
-                    result += subresult[i]
-                    i += 1
-                    is_encoded = True
-                    break
-            if not is_encoded:
-                result += char
         return result
     
     def decode(self):
         self._validate_key()
         result = ''
-        subresult = ''
         indent = 0
         parts: list[list] = self._parse_key()
-        all_chars = ''
-        for char in self.message:
-            for alphabet in alphabets.values():
-                if char in alphabet:
-                    all_chars += char
         for part in parts:
             for i in range(len(part)):
-                subresult += all_chars[indent + part.index(i+1)]
+                result += self.message[indent + part.index(i+1)]
             indent += len(part)
-        subresult += all_chars[len(subresult):]
-        i = 0
-        for char in self.message:
-            is_encoded = False
-            for alphabet in alphabets.values():
-                if char in alphabet:
-                    result += subresult[i]
-                    i += 1
-                    is_encoded = True
-                    break
-            if not is_encoded:
-                result += char
         return result
 
--- utils/alphabets.py
+++ utils/alphabets.py
@@ -1,4 +1,4 @@
 import json
 
-with open('C:\\Users\\mixas\\Documents\\repos\\Projects\\cryptography_pyqt\\utils\\alphs,json', 'r', encoding='utf-8') as f:
+with open('./utils/alphs.json', 'r', encoding='utf-8') as f:
     alphabets: dict[str, str] = json.load(f)
